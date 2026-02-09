0b028d467933b840ffed2bab3b4bd3aac26fe3d1
Bouba Mane Foot <contact@bouba-mane-foot.fr>
2026-02-09 15:47:08 +0100

    fix: increase z-index of lightbox modal to 9999

--- app/page.tsx
+++ app/page.tsx
@@ -262,7 +262,7 @@ export default function Home() {
       {/* MODAL LIGHTBOX pour afficher les images en grand */}
       {selectedImage && (
         <div 
-          className="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black bg-opacity-90 z-[9999] flex items-center justify-center p-4"
           onClick={() => setSelectedImage(null)}
         >
           <div className="relative max-w-7xl max-h-full">
